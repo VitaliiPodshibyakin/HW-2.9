d0aba18ad7a34e7f1e5c1e772e75b3f24a6a00c0
VitaliiPodshibyakin <vitalikpod@yandex.ru>
2022-03-18 18:38:10 +0300

    setup

--- HW-2.9/ViewController.swift
+++ HW-2.9/ViewController.swift
@@ -5,15 +5,33 @@
 //  Created by Виталий Подшибякин on 18.03.2022.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
+    
+    var animations = ["shake", "pop", "morph", "squeeze"]
+    var curves = ["spring", "linear", "easeIn", "easeOut", "easeInOut"]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    
+    @IBOutlet var springAnimationView: SpringView!
+    @IBOutlet var runSpringAnimationButton: SpringButton!
+    @IBOutlet var animationPropertiesLabel: UILabel!
+    
 
+    @IBAction func runSpringAnimation(_ sender: SpringButton) {
+        
+        springAnimationView.animation = animations.first ?? ""
+        springAnimationView.curve = curves.first ?? ""
+        springAnimationView.force = CGFloat.random(in: 0...2)
+        
+        
+        animationPropertiesLabel.text = "preset:\(animations.first ?? "") \n preset:\(curves.first ?? "")\n force: \(String(format: "%.2f", springAnimationView.force.magnitude))"
+        
+        springAnimationView.animate()
+        animations.shuffle()
+        curves.shuffle()
+        runSpringAnimationButton.setTitle(animations.first ?? "", for: .normal)
 
+        }
 }
 
